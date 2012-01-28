eb6d4cc9d424573c98c894f09bbd8b4d0ed71111
Marc Zych <marczych@gmail.com>
2012-01-27 23:45:23 -0800

    Add basic rotation of the snake

--- snake.lua
+++ snake.lua
@@ -3,20 +3,26 @@ local Vector = require "hump.vector"
 
 Snake = Class(function(self)
     self.position = Vector(1024 / 2, 768 / 2)
+    self.SPIN_RATE = 1
 end)
 
 function Snake:init()
     self.image = love.graphics.newImage("tmpart/ring.png")
+    self.rotation = 0;
 end
 
 function Snake:update(dt)
-
+   if love.mouse.isDown("l") then
+      self.rotation = self.rotation + self.SPIN_RATE * dt
+   elseif love.mouse.isDown("r") then
+      self.rotation = self.rotation - self.SPIN_RATE * dt
+   end
 end
 
 function Snake:draw()
     love.graphics.draw(self.image,
         self.position.x, self.position.y,
-        0,
+        self.rotation,
         1, 1,
         100, 100)
 end
