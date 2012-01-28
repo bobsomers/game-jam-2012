0f0203f139bcd5b7026cc4d1dd840abcc43f29b3
Katherine Blizard <kblizard@Zapdos.(none)>
2012-01-28 02:45:19 -0800

    trying to add bullets

---
+++ bullet.lua
@@ -0,0 +1,35 @@
+local Class = require "hump.class"
+local Vector = require "hump.vector"
+
+Bullet = Class(function(self)
+    self.position = Vector(1024 / 2, 768 / 2)
+    self.direction = Vector( 0, 1)
+    --self.SPIN_RATE = 0.3
+    self.rotation = 0
+    self.color = "grey"
+end)
+
+
+function Bullet:init()
+    self.image = love.graphics.newImage("tmpart/ring.png")
+end
+
+function Bullet:update(dt)	
+    self.position.x = self.position.x + self.direction.x*dt
+    self.position.y = self.position.y + self.direction.y*dt
+end
+
+function Bullet:draw()
+    love.graphics.draw(self.image,
+        self.position.x, self.position.y,
+        self.rotation,
+        .1, .1,
+        100, 100)
+end
+
+function Bullet:fire(startLocation, startdirection)
+	self.position = startLocation;
+	self.direction = startdirection;
+end
+
+
--- play_state.lua
+++ play_state.lua
@@ -1,16 +1,19 @@
 Gamestate = require "hump.gamestate"
+local Vector =  require "hump.vector"
 
 love.filesystem.load("snake.lua")()
+love.filesystem.load("bullet.lua")()
 
 play_state = Gamestate.new()
 local testPic = love.graphics.newImage("tmpart/plane.jpg");
 
 local snake = Snake()
-    
+local bullets = {}    
 function play_state:init()
     love.graphics.setBackgroundColor(255, 255, 255)
 
     snake:init()
+  
 end
 
 function play_state:enter(previous)
@@ -20,6 +23,11 @@ end
 function play_state:update(dt)
     snake:update(dt)
 
+    local i = 1
+    for i=1, #bullets, i + 1 do
+	bullets[i]:update(dt)
+    end 
+
 end
 
 function play_state:draw()
@@ -27,6 +35,11 @@ function play_state:draw()
     love.graphics.draw(testPic, 50, 50, 1, 1, 1, 0, 0);
 
     snake:draw()
+
+    local i = 1
+    for i=1, #bullets, i + 1 do
+	bullets[i]:draw()
+    end
 end
 
 function play_state:mousereleased(x, y, button)
@@ -34,5 +47,9 @@ function play_state:mousereleased(x, y, button)
         snake:rotateLeft()
     elseif button == "wu" then
         snake:rotateRight()
+    elseif button == "l" then
+	local bullet = Bullet();
+	bullet:fire(Vector(512, 512), Vector(0,1))
+	table.insert(bullets, bullet)
     end
 end
