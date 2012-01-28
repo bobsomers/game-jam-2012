3f50cc857e04697b031a675c19eead7f305e7319
Bob Somers <bob@bobsomers.com>
2012-01-27 19:58:09 -0800

    Basic hello world.

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    -- Game info.
+    t.title = "Planes on a Snake"
+    t.author = "CPGD"
+
+    -- Graphic settings.
+    t.screen.width = 1024
+    t.screen.height = 768
+end
---
+++ main.lua
@@ -0,0 +1,12 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print("Hello Planes on a Snake!", 100, 100)
+
+end
