307b14757ac8e27ed7ff3ee30b4a7ab4e83a5791
Taylor Arnicar <MutantFrk@gmail.com>
2012-01-28 02:14:55 -0800

    Add in a few sample orbiting planes.

---
+++ plane.lua
@@ -0,0 +1,81 @@
+-- Let's use the class mechanism from HUMP.
+local Class = require "hump.class"
+
+-- And the vector helper!
+local Vector = require "hump.vector"
+love.filesystem.load("utils.lua")()
+
+--[[
+color is the color of the plane.
+image is the already loaded image variable to be used for this plane
+r is how far the plane should start from the center of the snake, in pixels
+theta is the angle in radians 
+rSpeed is the speed at which it decreases its distance to the snake, in pixels
+    (this should be negative)
+thetaSpeed is the speed in radians / second at which the plane travels 
+    Positive thetaSpeed is counter-clockwise. Negative thetaSpeed = clockwise.
+]]
+Plane = Class(function(self, color, image, r, theta, rSpeed, thetaSpeed)
+   self.color = color;
+   self.image = image;
+   self.r = r;
+   self.theta = theta;
+   -- What are the base r and theta speeds of our plane?
+   self.rSpeed = rSpeed;
+   self.thetaSpeed = thetaSpeed;
+   self.health = 100;
+end)
+
+--[[
+function Plane:reset()
+   self.position = Vector(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4)
+   self.direction = Vector((math.random() * 2) - 1, (math.random() * 2) - 1)
+   self.direction:normalize_inplace()
+end
+]]--
+
+function Plane:update(dt)
+   -- Update the radius and theta
+   self.r = self.r + (dt * self.rSpeed);
+   self.theta = self.theta + (dt * self.thetaSpeed);
+end
+
+function Plane:draw()
+   local coordinates = polarToCartesian(self.r, self.theta);
+
+   -- We'll "scale" our image on the Y axis by either +1 or -1, depending on
+   -- which way the plane needs to be facing to look in his direction of travel.
+   local facing = 1
+   if self.thetaSpeed < 0 then
+       facing = -1
+   end
+
+   -- TODO: REPLACE THESE CONSTANTS
+   local centerX = 1024 / 2;
+   local centerY = 768 / 2;
+
+   local drawX = centerX + coordinates.x;
+   local drawY = centerY + coordinates.y;
+
+   local scale = 0.1;
+
+   -- Draw the plane at position (coordinate.x, coordinate.y) with no rotation,
+   -- 1.0 scale on the X axis and "facing" scale on the Y axis, 
+   -- and defining the center of our plane to be in the center of the image.
+   --(image, x, y, rotation, scaleX, scaleY, originOffsetX, originOffsetY)
+   love.graphics.draw(self.image, drawX, drawY, self.theta, scale, 
+    scale * facing, self.image:getWidth() / 2, self.image:getHeight() / 2);
+
+
+--[[
+    -- Draw the enemy at position (x, y) with no rotation, 1.0 scale on the Y
+    -- axis and "facing" scale on the X axis, and defining the center of our
+    -- enemy to be in the center of the image.
+    love.graphics.draw(self.images[self.level],
+        self.position.x, self.position.y,
+        0,
+        facing, 1,
+        self.images[self.level]:getWidth() / 2, self.images[self.level]:getHeight() / 2)
+  ]]
+end
+
--- play_state.lua
+++ play_state.lua
@@ -12,9 +12,9 @@ function play_state:init()
     love.graphics.setBackgroundColor(255, 255, 255)
 
     snake:init()
-   p = Plane ("red", testPic, 400, 0, -10, 1); 
-   p2 = Plane ("red", testPic, 400, 0, -10, 1); 
-   p3 = Plane ("red", testPic, 400, 0, -10, 1); 
+   p = Plane ("red", testPic, 350, 0, -10, 1); 
+   p2 = Plane ("red", testPic, 400, 0, -15, 1.3); 
+   p3 = Plane ("red", testPic, 450, 0, -20, -1); 
 end
 
 function play_state:enter(previous)
@@ -22,14 +22,19 @@ function play_state:enter(previous)
 end
 
 function play_state:update(dt)
-    snake:update(dt)
+   snake:update(dt)
+   p:update(dt)
+   p2:update(dt)
+   p3:update(dt)
 end
 
 function play_state:draw()
     love.graphics.print("Play state!", 100, 100)
-    love.graphics.draw(testPic, 50, 50, 1, 1, 1, 0, 0);
-
+    --love.graphics.draw(testPic, 50, 50, 1, 1, 1, 0, 0);
     snake:draw()
+    p:draw();
+    p2:draw();
+    p3:draw();
 end
 
 function play_state:mousereleased(x, y, button)
@@ -38,16 +43,4 @@ function play_state:mousereleased(x, y, button)
     elseif button == "wu" then
         snake:rotateRight()
     end
-
-   p:update(dt)
-   p2:update(dt)
-   p3:update(dt)
-end
-
-function play_state:draw()
-    love.graphics.print("Play state!", 100, 100);
-    p:draw();
-    p2:draw();
-    p3:draw();
-    --love.graphics.draw(testPic, 50, 50, 1, 1, 1, 0, 0);
 end
