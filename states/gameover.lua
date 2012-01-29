a17f809ef043364e8240946b31a6d0f60cd6c3cd
KATHYxx <kblizard@Zapdos.(none)>
2012-01-29 15:34:18 -0800

    CREDITS

--- states/gameover.lua
+++ states/gameover.lua
@@ -31,6 +31,8 @@ local snake = {}
 local score = 0
 local snakeHealth = 0
 
+local fadeOut = 0;
+
 -- Initialize the state. Called once when it's first created.
 function gameover:init()
    background = love.graphics.newImage("images/background.png")
@@ -44,11 +46,18 @@ function gameover:enter(previouis, hudd, oldplanes, Rscore)
    hud = hudd
    planes = oldplanes   
    score = Rscore
+   fadeOut = 0
 end
 
 -- Called when this state is updated.
 function gameover:update(dt)
-   
+   if fadeOut < 255 then
+	fadeOut = fadeOut + dt*20    
+   end
+
+   if fadeOut > 255 then
+      fadeOut = 255
+   end
 end
 
 -- Called when this state is drawn.
@@ -60,9 +69,25 @@ function gameover:draw()
    for i, plane in ipairs(planes) do
       plane:draw()
    end
+  
+   --fadeout
+   love.graphics.setColor(0,0,0,fadeOut)
+   love.graphics.rectangle("fill", 0,0, constants.SCREEN.x, constants.SCREEN.y )
+   love.graphics.setColor(255,255,255,255)
 
    love.graphics.draw(goLogo, constants.CENTER.x-300, 100)
 
+   if fadeOut > 100  then
+	   love.graphics.setColor(255,255,255, fadeOut-100)
+	   love.graphics.print("By", constants.CENTER.x-20, constants.CENTER.y + 20, 0, .4, .4 )
+	   love.graphics.print("Taylor Arnicar", constants.CENTER.x-50, constants.CENTER.y + 50, 0, .4, .4 )
+	   love.graphics.print("Katherine Blizard", constants.CENTER.x-10, constants.CENTER.y + 070, 0, .4, .4 )
+	   love.graphics.print("Sterling Hirsh", constants.CENTER.x-40, constants.CENTER.y + 100, 0, .4 ,.4 )
+	   love.graphics.print("Marc Zych", constants.CENTER.x+20, constants.CENTER.y + 130 , 0, .4 ,.4 )
+	   love.graphics.print("Bob Somers", constants.CENTER.x+50, constants.CENTER.y + 160 , 0, .4 ,.4 )
+	   love.graphics.print("Shirley Song", constants.CENTER.x+30, constants.CENTER.y + 190 , 0, .4 ,.4 )
+   end
+   love.graphics.setColor(255,255,255,255)
    hud:draw(0, score)
 end
 
