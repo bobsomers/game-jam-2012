f1cb1137b9ea1187aaed6b56b392b40286c8583e
Marc Zych <marczych@gmail.com>
2012-01-28 16:59:23 -0800

    Add sound stub class

--- main.lua
+++ main.lua
@@ -5,9 +5,12 @@ local Gamestate = require "hump.gamestate"
 local menu = require "states.menu"
 local play = require "states.play"
 local gameover = require "states.gameover"
+local Sound = require "sound"
 
 function love.load()
    -- Register the game state dispatcher and switch into the initial state.
    Gamestate.registerEvents()
    Gamestate.switch(play)
+   -- Load all sounds to be used in the game
+   Sound.init()
 end
---
+++ sound.lua
@@ -0,0 +1,9 @@
+local sound = {}
+
+-- Load all sounds here. Call only once
+function sound.init() 
+
+end
+
+-- Used for idiomatic module loading.
+return sound
