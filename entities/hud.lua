ed2bc746314776ef2fb4da46266eae657389697a
Katherine Blizard <kblizard@Zapdos.(none)>
2012-01-29 02:10:34 -0800

    made hud lightly better

--- entities/hud.lua
+++ entities/hud.lua
@@ -12,12 +12,11 @@ function Hud:update(dt)
 end
 
 function Hud:draw(snakehealth, score)
-   --score = 300
-   love.graphics.setColor(0,255,0)
+   love.graphics.setColor(0,0,0)
    love.graphics.rectangle("fill" , 0, constants.SCREEN.y-100, constants.SCREEN.x, 100)
-   tLevel = constants.SCREEN.y-50   --text level, the y level for the text
-   love.graphics.setColor(0,0,0);
-   love.graphics.printf("Score: "..score, 100, tLevel, 600, "left");
+   tLevel = constants.SCREEN.y-80   --text level, the y level for the text
+   love.graphics.setColor(255,255,255);
+   love.graphics.print("Score: "..score, 100, tLevel, 0, 2, 2);
    love.graphics.setColor(255,255,255)
 end
 
