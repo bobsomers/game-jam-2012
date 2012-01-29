d57985016afaf5c5ef9b434ee43d2ca9f434d794
Bob Somers <bob@bobsomers.com>
2012-01-29 09:45:36 -0800

    Health bar on the HUD.

--- entities/hud.lua
+++ entities/hud.lua
@@ -1,4 +1,5 @@
 local Class = require "hump.class"
+local Vector = require "hump.vector"
 local constants = require "constants"
 
 local Hud = Class(function(self)
@@ -9,6 +10,10 @@ local Hud = Class(function(self)
    self.fonts = {
       title = love.graphics.newFont("Assets/Defused.ttf", 24),
       score = love.graphics.newFont("Assets/WhiteRabbit.ttf", 56)
+   }
+   self.bar = {
+      position = Vector(constants.SCREEN.x - 250, constants.SCREEN.y - 50),
+      size = Vector(200, 30)
    }
 end)
 
@@ -23,12 +28,37 @@ function Hud:draw(snakehealth, score)
    love.graphics.setFont(self.fonts.title)
    love.graphics.setColor(255, 255, 255)
 
+   -- Titles.
    love.graphics.print("SCORE", 65, constants.SCREEN.y - 80)
    love.graphics.print("HEALTH", constants.SCREEN.x - 175, constants.SCREEN.y - 80)
 
+   -- Score.
    love.graphics.setFont(self.fonts.score)
-
    love.graphics.print(string.format("%06d", score), 50, constants.SCREEN.y - 45)
+
+   -- Heath bar.
+   love.graphics.setColor(255, 255, 255)
+   love.graphics.rectangle("fill",
+      self.bar.position.x, self.bar.position.y,
+      3, self.bar.size.y)
+   love.graphics.rectangle("fill",
+      self.bar.position.x + self.bar.size.x - 3, self.bar.position.y,
+      3, self.bar.size.y)
+   love.graphics.rectangle("fill",
+      self.bar.position.x, self.bar.position.y,
+      self.bar.size.x, 3)
+   love.graphics.rectangle("fill",
+      self.bar.position.x, self.bar.position.y + self.bar.size.y - 3,
+      self.bar.size.x, 3)
+
+   local width = (snakehealth / constants.SNAKE_MAX_HEALTH) * (self.bar.size.x - 6)
+
+   love.graphics.setColor(255, 80, 80)
+   love.graphics.rectangle("fill",
+      self.bar.position.x + 3, self.bar.position.y + 3,
+      width, self.bar.size.y - 6)
+
+   love.graphics.setColor(255, 255, 255)
 end
 
 -- idiomatic module loadinf
